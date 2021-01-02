21cc5f1668eed701383638664f3b27b242cbf00e
gabor <szabo.gabriel@gmail.com>
2021-01-02 15:17:03 +0100

    Change init process - start background jobs

--- PasswordPi/src/pi/password/Main.java
+++ PasswordPi/src/pi/password/Main.java
@@ -5,6 +5,9 @@ import com.jdi.ServiceFactoryImpl;
 
 import pi.password.config.JdiConfigService;
 import pi.password.gui.screenlock.ScreenlockController;
+import pi.password.service.hat.DisplayService;
+import pi.password.service.hat.KeyInputService;
+import pi.password.service.settings.SettingsService;
 
 public class Main {
 	
@@ -15,7 +18,25 @@ public class Main {
 	}
 	
 	public static void main(String[] args) throws ClassNotFoundException {
+		init();
 		getInstance(ScreenlockController.class).activate();
+	}
+	
+	private static void init() {
+		createHatInstance();		
+		startBackgroundJobs();
+	}
+	
+	private static void createHatInstance() {
+		// This isn't really necessary, it would be called transitively anyway
+		// but at least we are making clear, what is essential to be loaded.
+		getInstance(DisplayService.class);
+		getInstance(KeyInputService.class);
+	}
+	
+	private static void startBackgroundJobs() {
+		// The settings service takes care of the background jobs via callback.
+		getInstance(SettingsService.class);
 	}
 	
 }
